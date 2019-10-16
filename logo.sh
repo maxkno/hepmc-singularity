f068c3579125011b9a6ecb045df3b3609ee912aa
max.knobbe <max.knobbe@stud.uni-goettingen.de>
2019-10-16 19:03:43 +0200

    logo.sh
    adding cool logo

---
+++ logo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+RED='\033[0m'
+BLU='\033[0m'
+GRE='\033[0m'
+YEL='\033[0m'
+PUR='\033[0m'
+NCC='\033[0m'
+
+echo -e "    ${RED}             ${GRE}            ${PUR}             ${YEL}          ";
+echo -e "    ${RED}\$\$\      \$\$\ ${GRE}\$\$\         ${PUR}\$\$\      \$\$\ ${YEL} \$\$\$\$\$\$\  ";
+echo -e "    ${RED}\$\$\$\    \$\$\$ |${GRE}\$\$ |        ${PUR}\$\$\$\    \$\$\$ |${YEL}\$\$  __\$\$\ ";
+echo -e "    ${RED}\$\$\$\$\  \$\$\$\$ |${GRE}\$\$ |        ${PUR}\$\$\$\$\  \$\$\$\$ |${YEL}\$\$ /  \__|";
+echo -e "    ${RED}\$\$\\$\$\\$\$ \$\$ |${GRE}\$\$ |${NCC}\$\$\$\$\$\$\ ${PUR}\$\$\\$\$\\$\$ \$\$ |${YEL}\$\$ |      ";
+echo -e "    ${RED}\$\$ \\$\$\$  \$\$ |${GRE}\$\$ |${NCC}\______|${PUR}\$\$ \\$\$\$  \$\$ |${YEL}\$\$ |      ";
+echo -e "    ${RED}\$\$ |\\$  /\$\$ |${GRE}\$\$ |        ${PUR}\$\$ |\\$  /\$\$ |${YEL}\$\$ |  \$\$\ ";
+echo -e "    ${RED}\$\$ | \_/ \$\$ |${GRE}\$\$\$\$\$\$\$\$\   ${PUR}\$\$ | \_/ \$\$ |${YEL}\\$\$\$\$\$\$  |";
+echo -e "    ${RED}\__|     \__|${GRE}\________|  ${PUR}\__|     \__|${YEL} \______/ ";
+echo -e "    ${RED}             ${GRE}            ${PUR}             ${YEL}          ";
+echo -e "  ==${RED}=============${RED}============${RED}=============${RED}============";
+echo -e "    Singularity Image maintained by Max Knobbe"
+echo -e "    Source code available at: "
+echo -e "        https://gitlab.com/max.knobbe/hepmc-singularity "
+echo -e "    ${RED}             ${GRE}            ${PUR}             ${YEL}          ";
